0585fa1f5c276f526dc7cbeffe22801f5626b519
dianaxnav <dianamarie41098@gmail.com>
2023-08-18 14:27:14 -0700

    Initial commit

---
+++ R/burritos_cost.R
@@ -0,0 +1,13 @@
+#' Calculate tota burrito cost
+#'
+#' @param price price per burrito in $
+#' @param count number of burritos in order
+#'
+#' @return
+#' @export
+#'
+#' @examples
+burritos_cost <- function(price, count) {
+  price * count
+}
+
---
+++ R/chili.R
@@ -0,0 +1,12 @@
+#' Chili is the equation for force
+#'
+#' @param mass This is the mass
+#' @param acceleration This is the acceleration
+#'
+#' @return
+#' @export
+#'
+#' @examples
+chili <- function(mass, acceleration) {
+  mass * acceleration
+}
